ff8bbd416aaa226e4328e39e381c521e7c3b749f
sanjeev <www.jha.sanjeev@gmail.com>
2018-05-25 22:25:38 +0530

    Redux Saga Example Basic

---
+++ reduxsagabasic/src/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+
+
+import Counter from './Counter'
+import reducer from './reducers'
+import rootSaga from './sagas/saga';
+
+const sagaMiddleware = createSagaMiddleware()
+
+const store = createStore(
+    reducer,
+    applyMiddleware(sagaMiddleware)
+  )
+  sagaMiddleware.run(rootSaga)
+  
+  const action = type => store.dispatch({type})
+
+function render() {
+  ReactDOM.render(
+    <Counter
+      value={store.getState()}
+      onIncrement={() => action('INCREMENT')}
+      onDecrement={() => action('DECREMENT')}
+      onIncrementAsync={() => action('INCREMENT_ASYNC')} />,
+    document.getElementById('root')
+  )
+}
+
+render()
+store.subscribe(render)
